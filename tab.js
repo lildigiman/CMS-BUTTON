b2de7ba285db8b55bcc60471b6335f2dedb03874
unknown <koutnik_george@LAB-SSC203-09.roberts.edu>
2013-01-24 10:19:57 -0500

    Removed debugging console.log

--- tab.js
+++ tab.js
@@ -1,11 +1,9 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-	console.log('here');
 	document.addEventListener('click', function(e) {
 		e.preventDefault();
 		if (e.target && e.target.nodeName === 'A') {
-			//console.log(e.target);
 			chrome.tabs.create({url: e.target.href});
 		}
 	});
