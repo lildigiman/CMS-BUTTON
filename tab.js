731a75afa7ec6bc7db9af4a0273272cf02b5d5e1
SomeKittens <rkoutnik@gmail.com>
2013-01-24 14:14:38 -0500

    Loads options from localStorage

--- tab.js
+++ tab.js
@@ -1,10 +1,25 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
+  //Generate the link content
+  var localClasses = localStorage['classes']
+    , classList = document.getElementById('classList')
+    , classes
+    , classesString = '';
+  if(!localClasses) {
+    classList.innerHTML = 'You need to add some classes, mate.';
+  } else {
+    classes = JSON.parse(localClasses);
+    classes.forEach(function(item) {
+      classesString += '<a href="' + item[1] + '"><li>' + item[0] + '</li></a>\n';
+    });
+    classList.innerHTML = classesString
+  }
+  //Event delegator so we can have any number of links
 	document.addEventListener('click', function(e) {
 		e.preventDefault();
-		if (e.target && e.target.nodeName === 'A') {
-			chrome.tabs.create({url: e.target.href});
+		if (e.target && e.target.nodeName === 'LI') {
+			chrome.tabs.create({url: e.target.parentNode.href});
 		}
 	});
 });
